d1ed660a8575441edf163b87ef21d61136477076
etraister <eric.traister@gmail.com>
2017-05-03 23:21:23 -0400

    Try 2

--- Project3-CMPSC473-Spring2017/memoryManager.cpp
+++ Project3-CMPSC473-Spring2017/memoryManager.cpp
@@ -3,44 +3,38 @@
 
 unsigned long long memoryManager::memoryAccess(unsigned long long address)
 {
-	/*
-	1.	Is the page table empty?
-	2.	If not, is the page table full?
-	If(full)
-	-> fill the next page into the open table slot
-	Else(not full)
-	-> replace/swap page based on the active policy
-	*/
-	accessCounter++;
 	/* 
 	Add first entry to page table if empty 
 	*/
 	if (pageTable->isEmpty()) {		
-		accessCounter = 1;						// Reset memory access counter to initial post operation statez
-		newPageEntry(address);					// Create a new page entry
-		pageTable->push(page);					// Push the page to page table
+		index = 0;
+		newPageEntry(address, index);		// Create a new page entry
+		if (policy == FIFO)
+			pageTable->pushFIFO(page);			// Push the page to page table
+		else
+			pageTable->pushLRU(page);			// Push the page to page table
 	}
 	/*
 	If the page table is not full, continue adding pages
 	*/
-	else if (pageTable->size() < numFrames) {
-		//// Check if address already exists in page table
-		//for (int j = 0; j < pageTable->size(); j++) {
-		//	if (page->pageNum == (address / pow(2, N)))
-		//		return page->frameAddr;			// Return physical address if found in page table
-		//}
+	else if (index = pageTable->size() < numFrames) {
 		// Create new page entry if not in page table
-		newPageEntry(address);					// Create a new page entry
-		pageTable->push(page);					// Push the page to page table
+		newPageEntry(address, index);		// Create a new page entry
+		if (policy == FIFO)
+			pageTable->pushFIFO(page);			// Push the page to page table
+		else
+			pageTable->pushLRU(page);			// Push the page to page table
 	}
 	/*
 	Page table is full so return address if inside, otherwise invoke swap
 	*/
 	else {
 		// Check if address already exists in page table
-		for (int j = 0; j < pageTable->size(); j++) {
-			if (page->pageNum == (address / pow(2, N)))
-				return (page->frameRef + offset);			// Return physical address if found in page table
+		index = pageTable->inTable(address, N);
+		// Physical address found so return index + frameIndex
+		if (index != -1) {
+			int offset = address % pow(2, N);
+			return page->frameIndex + offset;
 		}
 		// Page table is full so swap
 		swap(address, page->pageNum);
@@ -54,47 +48,30 @@ void memoryManager::swap(unsigned int frameNumber, unsigned int pageNumber)
 	Page table is full so swap the current frame based on replacement policy
 	*/
 	if (policy == FIFO) {
-		// Find the page entry last entered into the table
-		for (int i = 0; i < pageTable->size(); i++) {
-			int lastIn = -1;
-			if (page->counter > lastIn) {
-				lastIn = page->counter;
-				pagenum = page->pageNum;
-			}
-		}
-		// Remove the last page entry and add in new frame to table
-		for (int j = 0; j < pageTable->size(); j++) {
-			page[j]->pop();
-			newPageEntry(frameNumber);
-			pageTable->push(*page);
-		}
+		index = page->frameIndex;				// Update index of physical memory location to be swapped
+		pageTable->popFIFO();					// Remove the last page entry
+		newPageEntry(frameNumber, index);		// Add in new frame to table
+		if (policy == FIFO)
+			pageTable->pushFIFO(page);			// Push the page to page table
+		else
+			pageTable->pushLRU(page);			// Push the page to page table
+
 	}
 	else if (policy == LRU) {
-		// Find the earliest page entry in the table
-		for (int i = 0; i < pageTable->size(); i++) {
-			int earliestIn = INT_MAX;
-			if (page->counter < earliestIn) {
-				earliestIn = page->counter;
-				pagenum = page->pageNum;
-			}
-		}
-		// Remove the earliest page entry and add in new frame to table
-		for (int j = 0; j < pageTable->size(); j++) {
-			if (page[j]->pageNum == pagenum) {
-				page[j]->pop();
-				newPageEntry(frameNumber);
-				pageTable->push(*page);
-			}
-		}
+		index = page->frameIndex;				// Update index of physical memory location to be swapped
+		pageTable->popLRU();					// Remove the earliest page entry
+		newPageEntry(frameNumber, index);		// Add in new frame to table
+		if (policy == FIFO)
+			pageTable->pushFIFO(page);			// Push the page to page table
+		else
+			pageTable->pushLRU(page);			// Push the page to page table
 	}
 }
 
-void memoryManager::newPageEntry(unsigned long long addr)
+void memoryManager::newPageEntry(unsigned long long addr, int frameIndex)
 {
 	PageEntry *page = (PageEntry *)malloc(sizeof(PageEntry));
 	page->pageNum = (addr / pow(2, N));
-	page->counter = accessCounter;
-	page->validBit = 1;
-	page->offset = (addr % pow(2, N));
-	page->frameRef = ?????;
+	page->frameIndex = index;
 }
+
--- Project3-CMPSC473-Spring2017/memoryManager.h
+++ Project3-CMPSC473-Spring2017/memoryManager.h
@@ -7,10 +7,8 @@
 struct PageEntry
 {
 	int pageNum;
-	int counter;
-	int validBit;
-	double frameRef;
-	double offset;
+	int frameIndex;
+	unsigned long long frameAddr;
 };
 struct PageTable
 {
@@ -23,18 +21,40 @@ struct PageTable
 	}
 
 	// Add new page to table
-	void push(PageEntry *page) {
+	void pushFIFO(PageEntry *page) {
 		table->push_back(*page);
 	}
 
+	// Add new page to table
+	void pushLRU(PageEntry *page) {
+		table->push_front(*page);
+	}
+
 	// Remove the last page and return it
-	PageEntry* pop() {
+	PageEntry* popFIFO() {
 		if (isEmpty())
 			return NULL;
 		PageEntry page = table->front();
-		free(&page);
+		//free(&page);
 		table->pop_front();
-		//return &page;
+	}
+
+	// Remove the last page and return it
+	PageEntry* popLRU() {
+		if (isEmpty())
+			return NULL;
+		PageEntry page = table->back();
+		//free(&page);
+		table->pop_back();
+	}
+
+	// Checks if page is in page table and return index to physical memory location if it is
+	int inTable(unsigned long long addr, unsigned int N) {
+		for (std::list<PageEntry>::iterator list_iter = table->begin(); list_iter != table->end(); list_iter++) {
+			if ((list_iter)->pageNum == (addr / pow(2, N)))
+				return (list_iter)->frameIndex;
+		}
+		return -1;	// returns -1 if not in page table
 	}
 
 	// Checks size of page table
@@ -54,20 +74,15 @@ public:
 	memoryManager(ReplacementPolicy p, unsigned int pS, unsigned int nF, unsigned int vA) : virtualMemoryManagerInterface(p, pS, nF, vA) {
 		pageTable = new PageTable();
 		policy = p;
-		pageSize = pS;
-		frameSize = nF;
-		vAddrSize = vA;
 	}
 	unsigned long long memoryAccess(unsigned long long address) override;
 	void swap(unsigned int frameNumber, unsigned int pageNumber);
-	void newPageEntry(unsigned long long addr);
+	void newPageEntry(unsigned long long addr, int frameIndex);
 
 private:
 	PageTable *pageTable = NULL;
-	int accessCounter = 0;
-
-	ReplacementPolicy policy;
-	unsigned int pageSize, frameSize, vAddrSize;
-	unsigned long long addr;
 	PageEntry *page = NULL;
+	ReplacementPolicy policy;
+	unsigned long long addr;
+	int index = -1;
 };
